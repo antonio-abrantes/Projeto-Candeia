0e181da93bdb2d168096a66195f2385853d13196
tonhus <antonio_abrantes@hotmail.com.br>
2017-11-05 13:27:41 -0300

    Envio de contato com Ajax 03

--- inc/email/contato_menor.php
+++ inc/email/contato_menor.php
@@ -27,6 +27,8 @@ $Vai            = "Data: ".implode("/", array_reverse(explode("-", $cliente->dad
                 "\n\nNome: ".$cliente->dados['nome']."\n\nTelefone: ".Funcoes_auxiliares::formataTelefone($cliente->dados['telefone']).
                 "\n\nE-mail: ".$cliente->dados['email']."\n\nMensagem: ".$cliente->dados['mensagem']."\n";
 
+//Grava no banco de dados
+$cliente->gravarCliente();
 
 // Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER),
 //o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
@@ -37,7 +39,7 @@ $response = [
 
  if (Envia_contato::smtpmailer('antonio.wac@gmail.com', $cliente->dados['email'], $enviador, 'Contato de Cliente', $Vai)) {
      //echo "<br>Enviado com sucesso...";
-     $cliente->gravarCliente();
+     //$cliente->gravarCliente();
      //echo "<meta http-equiv='Refresh' content='2;URL=./'>"; // Redireciona para uma página.
      $response['response'] = true;
  }else{
