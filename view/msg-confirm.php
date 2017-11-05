9ef3d92b746427d5a468d0bbb97310d4ab775ad4
tonhus <antonio_abrantes@hotmail.com.br>
2017-11-05 16:27:49 -0300

    Ajuste do modal confirmacao 11

--- view/msg-confirm.php
+++ view/msg-confirm.php
@@ -1,34 +1,32 @@
-<div id="div-confirmacao">
-    <div class="modal fade" id="modal-confirm" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Confirmação de envio</h4>
-                </div>
-                <div class="modal-body">
-                    Seus dados foram enviados com sucesso...<br> Breve entraremos em contato!
-                </div>
-                <div class="modal-footer">
-                    <button style="width: 100px" type="button" id="confirm" class="btn btn-default" data-dismiss="modal">OK</button>
-                </div>
+<div class="modal fade" id="modal-confirm" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <h4 class="modal-title" id="myModalLabel">Confirmação de envio</h4>
+            </div>
+            <div class="modal-body">
+                Seus dados foram enviados com sucesso...<br> Breve entraremos em contato!
+            </div>
+            <div class="modal-footer">
+                <button style="width: 100px" type="button" id="confirm" class="btn btn-default" data-dismiss="modal">OK</button>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="modal fade" id="modal-error" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Ops... Algo deu errado!</h4>
-                </div>
-                <div class="modal-body">
-                    Não foi possivel enviar seus dados... Tente novamente!
-                </div>
-                <div class="modal-footer">
-                    <button style="width: 100px" type="button" class="btn btn-default" data-dismiss="modal">OK</button>
-                </div>
+<div class="modal fade" id="modal-error" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <h4 class="modal-title" id="myModalLabel">Ops... Algo deu errado!</h4>
+            </div>
+            <div class="modal-body">
+                Não foi possivel enviar seus dados... Tente novamente!
+            </div>
+            <div class="modal-footer">
+                <button style="width: 100px"  type="button" class="btn btn-default" data-dismiss="modal">OK</button>
             </div>
         </div>
     </div>
