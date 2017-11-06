6fba9a4805b0e22780f6e6dab6a9e69744b613e5
tonhus <antonio_abrantes@hotmail.com.br>
2017-11-06 18:02:21 -0300

    Teste com rota heroku 01

--- view/modal-contato.php
+++ view/modal-contato.php
@@ -10,7 +10,7 @@
                 <h4 class="modal-title">Envie sua dúvida ou sugestão.</h4>
             </div>
             <div class="modal-body">
-                <form class="form" id="form-contato" name='envia_mail' method="post" action="contato-menor-true">
+                <form class="form" id="form-contato" name='envia_mail' method="POST" action="https://candeia-studio.herokuapp.com/contato-menor-true">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
