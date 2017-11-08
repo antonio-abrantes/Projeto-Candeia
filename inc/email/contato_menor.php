2c5baf689226d823d1a0e49b4dabbac61b9f0d78
tonhus <antonio_abrantes@hotmail.com.br>
2017-11-08 20:51:45 -0300

    Correcao banco de dados 04

--- inc/email/contato_menor.php
+++ inc/email/contato_menor.php
@@ -1,8 +1,8 @@
 <?php
 
 mb_internal_encoding("UTF-8");
-mb_http_output( "iso-8859-1" );
-ob_start("mb_output_handler");
+//mb_http_output( "iso-8859-1" );
+//ob_start("mb_output_handler");
 header("Content-Type: text/html; charset=ISO-8859-1",true);
 //header('Content-Type: text/html; charset=utf-8');
 
