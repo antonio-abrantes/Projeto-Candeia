5c5226d78fce610551b1542b62dccd4f3d2427e8
tonhus <antonio_abrantes@hotmail.com.br>
2017-11-09 23:14:07 -0300

    Correcao POST contato_menor

--- inc/email/contato_menor.php
+++ inc/email/contato_menor.php
@@ -1,52 +1,58 @@
 <?php
 
-//header("Content-Type: text/html; charset=iso-8859-1");
+header("Content-Type: text/html; charset=UTF-8");
 
 require '../../inc/classes/Funcoes_auxiliares.php';
 require '../../inc/classes/Cliente.php';
 require 'Envia_contato.php';
 
-    $cliente = new Cliente();
-
-    $data = $_POST['contato'];
-
-$cliente->dados['nome']         = $data["inputNome"];
-$cliente->dados['email']        = $data["inputEmail"];
-$cliente->dados['mensagem']     = $data["mensagem"];
-$cliente->dados['telefone']     = Funcoes_auxiliares::limpaEspacosBranco($data["inputTel"]);
-$cliente->dados['cad_data']     = $data["inputData"];
-
-//echo json_encode($cliente);
-
-$enviador = substr($cliente->dados['nome'], 0, strpos($cliente->dados['nome'], " "));
-
-
-// Variável que junta os valores acima e monta o corpo do email
-
-$Vai            = "Data: ".implode("/", array_reverse(explode("-", $cliente->dados['cad_data']))).
-                "\n\nNome: ".$cliente->dados['nome']."\n\nTelefone: ".Funcoes_auxiliares::formataTelefone($cliente->dados['telefone']).
-                "\n\nE-mail: ".$cliente->dados['email']."\n\nMensagem: ".$cliente->dados['mensagem']."\n";
-
-$cliente->gravarCliente();
-
-// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER),
-//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
-
 $response = [
     "response"
 ];
 
- if (Envia_contato::smtpmailer('antonio.wac@gmail.com', $cliente->dados['email'], $enviador, 'Contato de Cliente', $Vai)) {
-     //echo "<br>Enviado com sucesso...";
-     //$cliente->gravarCliente();
-     //echo "<meta http-equiv='Refresh' content='2;URL=./'>"; // Redireciona para uma página.
-     $response['response'] = true;
- }else{
-     $response['response'] = false;
- }
- //if (!empty($error)) echo $error;
+if(isset($_POST) && count($_POST) > 0){
 
-echo json_encode($response);
+    $cliente = new Cliente();
 
+    $data = $_POST['contato'];
+
+    $cliente->dados['nome']         = $data["inputNome"];
+    $cliente->dados['email']        = $data["inputEmail"];
+    $cliente->dados['mensagem']     = $data["mensagem"];
+    $cliente->dados['telefone']     = Funcoes_auxiliares::limpaEspacosBranco($data["inputTel"]);
+    $cliente->dados['cad_data']     = $data["inputData"];
+
+//echo json_encode($cliente);
+
+    $enviador = substr($cliente->dados['nome'], 0, strpos($cliente->dados['nome'], " "));
+
+
+// Variável que junta os valores acima e monta o corpo do email
+
+    $Vai            = "Data: ".implode("/", array_reverse(explode("-", $cliente->dados['cad_data']))).
+        "\n\nNome: ".$cliente->dados['nome']."\n\nTelefone: ".Funcoes_auxiliares::formataTelefone($cliente->dados['telefone']).
+        "\n\nE-mail: ".$cliente->dados['email']."\n\nMensagem: ".$cliente->dados['mensagem']."\n";
+
+    $cliente->gravarCliente();
+
+// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER),
+//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
+
+
+    if (Envia_contato::smtpmailer('antonio.wac@gmail.com', $cliente->dados['email'], $enviador, 'Contato de Cliente', $Vai)) {
+        //echo "<br>Enviado com sucesso...";
+        //$cliente->gravarCliente();
+        $response['response'] = true;
+    }else{
+        $response['response'] = false;
+    }
+    //if (!empty($error)) echo $error;
+
+    echo json_encode($response);
+
+}else{
+    echo "Área restrita...";
+    echo "<meta http-equiv='Refresh' content='0;URL=../../'>"; // Redireciona para uma página para o index.php.
+}
 
 ?>
--- inc/email/orcamento.php
+++ inc/email/orcamento.php
@@ -1,109 +1,112 @@
 <?php
 
-//header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: text/html; charset=UTF-8');
 
 require 'inc/classes/Funcoes_auxiliares.php';
 require 'inc/classes/ClienteOrcamento.php';
 require 'Envia_orcamento.php';
 
+$confirmacao = false;
+
+if(isset($_POST) && count($_POST) > 0) {
+
     $cliente = new ClienteOrcamento();
 
 
-$cliente->dados['nome']         = $_POST["inputNomeOrc"];
-$cliente->dados['email']        = $_POST["inputEmailOrc"];
-$cliente->dados['telefone']     = Funcoes_auxiliares::limpaEspacosBranco($_POST["inputTelOrc"]);
-$cliente->dados['cad_data']     = $_POST["inputDataOrc"];
+    $cliente->dados['nome'] = $_POST["inputNomeOrc"];
+    $cliente->dados['email'] = $_POST["inputEmailOrc"];
+    $cliente->dados['telefone'] = Funcoes_auxiliares::limpaEspacosBranco($_POST["inputTelOrc"]);
+    $cliente->dados['cad_data'] = $_POST["inputDataOrc"];
 
 
-$enviador = substr($cliente->dados['nome'], 0, strpos($cliente->dados['nome'], " "));
+    $enviador = substr($cliente->dados['nome'], 0, strpos($cliente->dados['nome'], " "));
 
-$cabeçalho = "Data: ".implode("/", array_reverse(explode("-", $cliente->dados['cad_data']))).
-             "\n\nNome: ".$cliente->dados['nome']."\n\nTelefone: ".Funcoes_auxiliares::formataTelefone($cliente->dados['telefone']).
-             "\n\nE-mail: ".$cliente->dados['email']."\n\n";
+    $cabeçalho = "Data: " . implode("/", array_reverse(explode("-", $cliente->dados['cad_data']))) .
+        "\n\nNome: " . $cliente->dados['nome'] . "\n\nTelefone: " . Funcoes_auxiliares::formataTelefone($cliente->dados['telefone']) .
+        "\n\nE-mail: " . $cliente->dados['email'] . "\n\n";
 
-$tipoDeProjeto = [];
+    $tipoDeProjeto = [];
 
-if(isset($_POST["identVisual"])){
-    array_push($tipoDeProjeto, $_POST["identVisual"]);
-}
-if(isset($_POST["designInterior"])){
-    array_push($tipoDeProjeto, $_POST["designInterior"]);
-}
-if(isset($_POST["marcaSimples"])){
-    array_push($tipoDeProjeto, $_POST["marcaSimples"]);
-}
-if(isset($_POST["opProduto"])){
-    array_push($tipoDeProjeto, $_POST["opProduto"]);
-}
+    if (isset($_POST["identVisual"])) {
+        array_push($tipoDeProjeto, $_POST["identVisual"]);
+    }
+    if (isset($_POST["designInterior"])) {
+        array_push($tipoDeProjeto, $_POST["designInterior"]);
+    }
+    if (isset($_POST["marcaSimples"])) {
+        array_push($tipoDeProjeto, $_POST["marcaSimples"]);
+    }
+    if (isset($_POST["opProduto"])) {
+        array_push($tipoDeProjeto, $_POST["opProduto"]);
+    }
 
-$projetos = "";
+    $projetos = "";
 
-foreach ($tipoDeProjeto as $tipo){
-    $projetos .= " * ".$tipo."\n";
-}
+    foreach ($tipoDeProjeto as $tipo) {
+        $projetos .= " * " . $tipo . "\n";
+    }
 
-if(count($tipoDeProjeto) != 0){
-    $projetos = "\n# Tipos de projetos\n\n".$projetos;
-}
+    if (count($tipoDeProjeto) != 0) {
+        $projetos = "\n# Tipos de projetos\n\n" . $projetos;
+    }
 
-$camposTexto = [];
+    $camposTexto = [];
 
-if(strlen($_POST["campoAtuacao"]) > 0){
-    array_push($camposTexto, "# Campo de atuação da empresa\n".$_POST["campoAtuacao"]);
-}
-if(strlen($_POST["historiaEmpresa"]) > 0){
-    array_push($camposTexto, "# Historia da Empresa\n".$_POST["historiaEmpresa"]);
-}
-if(strlen($_POST["missaoValores"]) > 0){
-    array_push($camposTexto, "# Missão de valores\n".$_POST["missaoValores"]);
-}
-if(strlen($_POST["publicoDesejado"]) > 0){
-    array_push($camposTexto, "# Público desejado\n".$_POST["publicoDesejado"]);
-}
-if(strlen($_POST["objetivoNegocio"]) > 0){
-    array_push($camposTexto, "# Adjetivo que define o negócio\n".$_POST["objetivoNegocio"]);
-}
-if(strlen($_POST["possuiSlogan"]) > 0){
-    array_push($camposTexto, "# Slogan\n".$_POST["possuiSlogan"]);
-}
-if(strlen($_POST["msgParaPassar"]) > 0){
-    array_push($camposTexto, "# Mensagem que deseja passar\n".$_POST["msgParaPassar"]);
-}
-if(strlen($_POST["produtosNoMercado"]) > 0){
-    array_push($camposTexto, "# Produtos no mercado\n".$_POST["produtosNoMercado"]);
-}
-if(strlen($_POST["principaisConcorrentes"]) > 0){
-    array_push($camposTexto, "# Principais Concorrentes\n".$_POST["principaisConcorrentes"]);
-}
-if(strlen($_POST["produtosParaDesenvolver"]) > 0){
-    array_push($camposTexto, "# Produtos a serem desenvolvidos\n".$_POST["produtosParaDesenvolver"]);
-}
-if(strlen($_POST["preferenciaDeMaterial"]) > 0){
-    array_push($camposTexto, "# Material preferencial\n".$_POST["preferenciaDeMaterial"]);
-}
-if(strlen($_POST["restricao"]) > 0){
-    array_push($camposTexto, "# Restrições\n".$_POST["restricao"]);
-}
-if(strlen($_POST["dataDeLancamento"]) > 0){
-    array_push($camposTexto, "# Data para lançamento\n".$_POST["dataDeLancamento"]);
-}
-if(strlen($_POST["limiteDeOrcamento"]) > 0){
-    array_push($camposTexto, "# Limitação do orçamento\n".$_POST["limiteDeOrcamento"]);
-}
-if(strlen($_POST["informExtras"]) > 0){
-    array_push($camposTexto, "# Informações extras\n".$_POST["informExtras"]);
-}
+    if (strlen($_POST["campoAtuacao"]) > 0) {
+        array_push($camposTexto, "# Campo de atuação da empresa\n" . $_POST["campoAtuacao"]);
+    }
+    if (strlen($_POST["historiaEmpresa"]) > 0) {
+        array_push($camposTexto, "# Historia da Empresa\n" . $_POST["historiaEmpresa"]);
+    }
+    if (strlen($_POST["missaoValores"]) > 0) {
+        array_push($camposTexto, "# Missão de valores\n" . $_POST["missaoValores"]);
+    }
+    if (strlen($_POST["publicoDesejado"]) > 0) {
+        array_push($camposTexto, "# Público desejado\n" . $_POST["publicoDesejado"]);
+    }
+    if (strlen($_POST["objetivoNegocio"]) > 0) {
+        array_push($camposTexto, "# Adjetivo que define o negócio\n" . $_POST["objetivoNegocio"]);
+    }
+    if (strlen($_POST["possuiSlogan"]) > 0) {
+        array_push($camposTexto, "# Slogan\n" . $_POST["possuiSlogan"]);
+    }
+    if (strlen($_POST["msgParaPassar"]) > 0) {
+        array_push($camposTexto, "# Mensagem que deseja passar\n" . $_POST["msgParaPassar"]);
+    }
+    if (strlen($_POST["produtosNoMercado"]) > 0) {
+        array_push($camposTexto, "# Produtos no mercado\n" . $_POST["produtosNoMercado"]);
+    }
+    if (strlen($_POST["principaisConcorrentes"]) > 0) {
+        array_push($camposTexto, "# Principais Concorrentes\n" . $_POST["principaisConcorrentes"]);
+    }
+    if (strlen($_POST["produtosParaDesenvolver"]) > 0) {
+        array_push($camposTexto, "# Produtos a serem desenvolvidos\n" . $_POST["produtosParaDesenvolver"]);
+    }
+    if (strlen($_POST["preferenciaDeMaterial"]) > 0) {
+        array_push($camposTexto, "# Material preferencial\n" . $_POST["preferenciaDeMaterial"]);
+    }
+    if (strlen($_POST["restricao"]) > 0) {
+        array_push($camposTexto, "# Restrições\n" . $_POST["restricao"]);
+    }
+    if (strlen($_POST["dataDeLancamento"]) > 0) {
+        array_push($camposTexto, "# Data para lançamento\n" . $_POST["dataDeLancamento"]);
+    }
+    if (strlen($_POST["limiteDeOrcamento"]) > 0) {
+        array_push($camposTexto, "# Limitação do orçamento\n" . $_POST["limiteDeOrcamento"]);
+    }
+    if (strlen($_POST["informExtras"]) > 0) {
+        array_push($camposTexto, "# Informações extras\n" . $_POST["informExtras"]);
+    }
 
-$camposPreenchidos = "\n";
+    $camposPreenchidos = "\n";
 
-foreach ($camposTexto as $campo){
-    $camposPreenchidos .= $campo."\n";
-}
-$camposPreenchidos .= "\n";
+    foreach ($camposTexto as $campo) {
+        $camposPreenchidos .= $campo . "\n";
+    }
+    $camposPreenchidos .= "\n";
 
-$Vai = $cabeçalho.$projetos.$camposPreenchidos;
+    $Vai = $cabeçalho . $projetos . $camposPreenchidos;
 
-//$confirmacao = false;
 
 //if (Envia_orcamento::smtpmailer('antonio.wac@gmail.com', $cliente->dados['email'], $enviador, utf8_encode('Orcamento'), $Vai)) {
     $cliente->gravarCliente();
@@ -111,4 +114,4 @@ $Vai = $cabeçalho.$projetos.$camposPreenchidos;
 //    //echo "<meta http-equiv='Refresh' content='2;URL=./'>"; // Redireciona para uma página.
 //}
 //if (!empty($error)) echo $error;
-
+}
